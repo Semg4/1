d0aee67e661508e04f30fedfba518092600d0721
1-МД-8 КАЛАШНИКОВ <azx163azx@gmail.com>
2022-03-01 15:21:18 +0300

    update

---
+++ bot.py
@@ -0,0 +1,6 @@
+import telebot
+bot = telebot.TeleBot(5173440304:AAGOO6PhsDxOdKwCfpeOgHwPquwRd90jc3w)
+@bot.message_handler(commands=["start"])
+def start(message, res = False):
+    chat_id = message.chat.id
+    bot.send_message(chat.id, text)
